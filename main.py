ac21f254a2bcee4817f3a220e4c17b6f918d1f71
Cole Challand <ctchalland@gmail.com>
2022-01-23 09:19:14 -0500

    added basic logger

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-import time
-import pandas as pd
+import tkinter as tk
+import logging
 
-print(time.time())
-pd.DataFrame()
+logger = logging.getLogger()
+
+logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+logger.debug("This message is important only when debugging the program")
+logger.info("This message just shows basic information")
+logger.warning("This message is about something you should pay attention to")
+logger.error("This message helps to debug an error that occurred in your program")
+
+root = tk.Tk()
+root.mainloop()
