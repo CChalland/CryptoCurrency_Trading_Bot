d755d1e531968b109cf0f2684df4e59b24cce4fd
Cole Challand <ctchalland@gmail.com>
2022-01-31 08:17:29 -0500

    added broiler plate for logging component

--- interface/root_component.py
+++ interface/root_component.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 import logging
+import time
 
 from connectors.bitmex import BitmexClient
 from connectors.binance import BinanceFuturesClient
 
 from interface.styling import *
-# from interface.logging_component import Logging
+from interface.logging_component import Logging
 # from interface.watchlist_component import Watchlist
 # from interface.trades_component import TradesWatch
 # from interface.strategy_component import StrategyEditor
@@ -24,8 +25,15 @@ class Root(tk.Tk):
         self.title("Trading Bot")
         self.configure(bg=BG_COLOR)
 
-        self.left_frame(tk.Frame(self, bg=BG_COLOR))
-        self.left_frame.pack(side=tk.LEFT)
+        self._left_frame = tk.Frame(self, bg=BG_COLOR)
+        self._left_frame.pack(side=tk.LEFT)
         
-        self.right_frame(tk.Frame(self, bg=BG_COLOR))
-        self.right_frame.pack(side=tk.LEFT)
+        self._right_frame = tk.Frame(self, bg=BG_COLOR)
+        self._right_frame.pack(side=tk.LEFT)
+        
+        self._logging_frame = Logging(self._left_frame, bg=BG_COLOR)
+        self._logging_frame.pack(side=tk.TOP)
+        
+        self._logging_frame.add_log("This is a text message")
+        time.sleep(2)
+        self._logging_frame.add_log("second message")
