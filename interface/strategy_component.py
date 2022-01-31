424b13bad915ac0bd94f201997e0e48a33827907
Cole Challand <ctchalland@gmail.com>
2022-01-31 18:39:33 -0500

    added broiler plate for trade/strategy components

--- interface/strategy_component.py
+++ interface/strategy_component.py
@@ -1,7 +1,11 @@
 import tkinter as tk
+import typing
 
 from interface.styling import *
 
+from connectors.binance import BinanceFuturesClient
+from connectors.bitmex import BitmexClient
+
 class StrategyEditor(tk.Frame):
-     def __init__(self, *args, **kwargs):
+     def __init__(self, root, binance: BinanceFuturesClient, bitmex: BitmexClient, *args, **kwargs):
         super().__init__(*args, **kwargs)
